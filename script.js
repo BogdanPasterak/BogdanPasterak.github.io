963b8a1d15d90aa82bd2157166e8fc0548c4c1ec
Bogdan <bogdanpasterak@gmail.com>
2025-08-24 16:39:57 +0100

    tlo i cell

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // Tutaj możesz dodać kod JavaScript, który będzie sterował funkcjonalnością strony
-// na przykład obsługę przycisków, animacje, itp.
+const board = document.getElementById('board');
+
+
+// zmienne
+
+let square = 0;
 
 console.log("Strona załadowana!");
+
+function resizeBoard() {
+    // Wyliczenie kratki
+    board.style.width = board.style.height = "";
+
+    const rect = board.getBoundingClientRect();
+    square = Math.floor(rect.width / 7);
+
+    // Poprawka rozmiaru panelu
+    board.style.width = (square * 7) + 'px';
+    board.style.height = (square * 8) + 'px';
+
+    // Ustawienie rozmiarów siatki dla panelu
+    board.style.gridTemplateColumns = `repeat(7, ${square}px)`;
+    board.style.gridTemplateRows = `repeat(8, ${square}px)`;
+
+}
+
+
+
+window.addEventListener('DOMContentLoaded', resizeBoard);
+window.addEventListener('resize', resizeBoard);
